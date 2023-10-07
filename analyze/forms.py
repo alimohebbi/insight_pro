0bce076d86d0d54fe8cb11c1dd2ea7b0938f7da8
ali mohebbi <ali.mohebbi@usi.ch>
2023-10-07 13:34:19 +0200

    Ready for deploy

--- analyze/forms.py
+++ analyze/forms.py
@@ -4,5 +4,6 @@ from django import forms
 
 class WebURLForm(forms.Form):
     target_url = forms.URLField(max_length=200,
-                                label='Target URL', help_text="Please enter url of a website for analyzing.",
+                                label='',
+                                help_text="Please enter the address of an English-language website for analysis.",
                                 widget=forms.TextInput(attrs={'placeholder': 'https://example.com'}))
